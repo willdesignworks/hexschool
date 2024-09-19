0acb7a1b7fad4012fe8396d3e16a8b7c1cdb578d
willdesignworks <willbqool@gmail.com>
2024-09-19 14:31:12 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/login">登入</router-link> |
+    <router-link to="/dashboard">dashboard</router-link> |
+    <router-link to="/dashboard/products">products</router-link> |
+  </nav>
+  <router-view/>
+
+</template>
+
+<style lang="scss">
+//@import "~bootstrap/scss/bootstrap";
+@import "./assets/all.scss";
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button"
+     data-bs-toggle="collapse"
+     data-bs-target="#navbarNav"
+     aria-controls="navbarNav"
+     aria-expanded="false"
+     aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" @click.prevent="logout">登出</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      // api 路徑
+      const api = `${process.env.VUE_APP_API}logout`;// console.log('api');
+      // api方法
+      this.$http.post(api, this.user).then((res) => {
+        if (res.data.success) {
+          // 轉止到驗證畫面
+          this.$router.push('/login');
+        }
+      });
+    },
+  },
+};
+</script>
